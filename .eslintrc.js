7e58e1f61a1042717f577a40205e93a6b62ef427
Kevin Kanzelmeyer <kevin.kanzelmeyer@archarithms.com>
2017-01-09 12:59:02 -0600

    added flow support for lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,25 @@
 module.exports = {
-    "extends": "standard",
-    "plugins": [
-        "react",
-        "react-native",
-        "standard",
-        "promise",
+  "extends": ["standard", "plugin:flowtype/recommended"],
+  "plugins": [
+    "react", "react-native", "standard", "promise", "flowtype"
+  ],
+  "parserOptions": {
+    "ecmaVersion": 7,
+    "sourceType": "module",
+    "ecmaFeatures": {
+      "jsx": true
+    }
+  },
+  "rules": {
+    "react/jsx-uses-react": "error",
+    "react/jsx-uses-vars": "error",
+    "flowtype/boolean-style": [
+      2, "boolean"
     ],
-    "parserOptions": {
-      "ecmaVersion": 7,
-      "sourceType": "module",
-      "ecmaFeatures": {
-        "jsx": true
-      }
-    },
-    "rules": {
-       "react/jsx-uses-react": "error",
-       "react/jsx-uses-vars": "error",
-     }
-};
+  },
+  "settings": {
+    "flowtype": {
+      "onlyFilesWithFlowAnnotation": true
+    }
+  }
+}
